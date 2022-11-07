067b3cad34c4b53e568c806047e83316fdd41c72
Clark Chan <clarkchan@Clarks-MacBook-Pro.local>
2022-11-07 16:01:49 -0500

    feat: add paths object to tsconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path, { resolve } from 'path'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  //   resolve: {
-  //     alias: {
-  //       '@': path.resolve(__dirname, './src'),
-  //     },
-  //   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
