b4d4459a40a09e4b84c8b058a8e76f12bb886d0d
Clark Chan <clarkchan@Clarks-MacBook-Pro.local>
2022-11-08 09:58:47 -0500

    feat: properly configured vite alias for @ imports

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import ReactGA from 'react-ga'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { getEnvironment } from 'utils'
+import { getEnvironment } from '@/utils'
 
 window.addEventListener('beforeinstallprompt', e => {
   e.preventDefault()
