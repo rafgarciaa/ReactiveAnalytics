44fde5bb5453c563809903bcad0d92b4ca03a542
Clark Chan <clarkchan@Clarks-MacBook-Pro.local>
2022-11-14 11:53:26 -0500

    chore: remove dist folder

--- client/dist/assets/index.57c82592.js
+++
@@ -1,1 +0,0 @@
-import{P as s}from"./index.d3dae8df.js";class e extends s{constructor(){super(...arguments),this.name="pwa",this.type="desktop"}}export{e as PWA};
--- client/dist/assets/index.da9c0d1c.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./index.d3dae8df.js";export{e as Browser};
