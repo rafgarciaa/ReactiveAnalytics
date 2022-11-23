f275a2ffb236ac51fc25ddff26e13f04bba43fea
Clark Chan <clarkchan@Clarks-MacBook-Pro.local>
2022-11-23 15:30:33 -0500

    chore: complete functionality for copying templates to /public and /dist

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import graphql from '@rollup/plugin-graphql'
 import copy from 'rollup-plugin-copy'
 
-const copyOpenfinPlugin = (dev: boolean) => {
+const copyOpenfinPlugin = (dev: boolean):Plugin => {
   const scheme = process.env.HTTPS === 'true' ? 'https' : 'http'
   const hostUrl = `${scheme}://${process.env.HOST || 'localhost'}:${process.env.PORT || '3005'}`
   const env = process.env.ENVIRONMENT_NAME || 'local'
+
+  const transform = dev
+    ? contents => {
+        const transformed = contents
+          .toString()
+          .replace(/{\*host_url\*}/g, hostUrl)
+          .replace(/{{environment}}/g, env)
+          .replace(/{{environment_suffix}}/g, env === 'prod' ? '' : env.toUpperCase())
+        return transformed
+      }
+    : undefined
+
   return {
     ...copy({
       targets: [
         {
           src: `./public-openfin/app.json`,
-          dest: './dist/openfin',
-          transform: contents => {
-            const transformed = contents
-              .toString()
-              .replace(/{\*host_url\*}/g, hostUrl)
-              .replace(/{{environment}}/g, env)
-              .replace(/{{environment_suffix}}/g, env === 'prod' ? '' : env.toUpperCase())
-            return transformed
-          },
+          dest: dev ? './public' : './dist/openfin',
+          transform,
         },
       ],
       verbose: true,
@@ -32,18 +37,22 @@ const copyOpenfinPlugin = (dev: boolean) => {
   }
 }
 
-const copyWebManifestPlugin = (dev: boolean) => {
+const copyWebManifestPlugin = (dev: boolean):Plugin => {
+  const transform = dev
+    ? contents =>
+        contents
+          .toString()
+          // We don't want to show PROD in the PWA name
+          .replace(/{{environment_suffix}}/g, dev ? ' (DEV)' : '')
+    : undefined
+
   return {
     ...copy({
       targets: [
         {
           src: './public-pwa/manifest.json',
           dest: dev ? './public' : './dist',
-          transform: contents =>
-            contents
-              .toString()
-              // We don't want to show PROD in the PWA name
-              .replace(/{{environment_suffix}}/g, dev ? ' (DEV)' : ''),
+          transform,
         },
       ],
       verbose: true,
